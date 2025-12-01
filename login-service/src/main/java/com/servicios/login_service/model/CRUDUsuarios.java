e38af47f9c027642d1ef3173c390ce990cec26e5
pierre <Administrador@192.168.1.20>
2025-11-30 21:22:02 -0500

    L

--- login-service/src/main/java/com/servicios/login_service/model/CRUDUsuarios.java
+++
@@ -1,9 +0,0 @@
-package com.servicios.login_service.model;
-
-import com.servicios.login_service.model.DTOUsuario;
-import java.util.LinkedList;
-
-public interface CRUDUsuarios {
-    public LinkedList<DTOUsuario> ListarUsuarios();
-    DTOUsuario ValidarSesion(String correo, String contra);
-}
