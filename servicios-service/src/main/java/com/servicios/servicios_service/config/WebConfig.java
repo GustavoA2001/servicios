04ba59a046d82e3d1d608442f09ee33de72992bf
Lesly Sharom <lesly.sharom@gmail.com>
2025-12-09 00:54:43 -0500

    correcion errores conexion

--- servicios-service/src/main/java/com/servicios/servicios_service/config/WebConfig.java
+++ servicios-service/src/main/java/com/servicios/servicios_service/config/WebConfig.java
@@ -20,6 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(clienteAuthInterceptor)
-                .addPathPatterns("/**"); // protege todas las rutas
+                .addPathPatterns("/**") // protege todas las rutas
+                .excludePathPatterns("/servicios/**");
     }
 }
